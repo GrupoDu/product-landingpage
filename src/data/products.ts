c745994a566c4b3a302d234eb2a26213660cbe75
jukalbf <lgbr212@gmail.com>
2025-08-29 13:16:19 -0300

    chore: dados de produtos e tipos

---
+++ src/data/products.ts
@@ -0,0 +1,22 @@
+import { Product } from "./types";
+
+// Imagens
+import CarroDeMao from "@/assets/produtos/Carro de mão.png";
+import CarroJerica160 from "@/assets/produtos/Carro jerica 160L.png";
+
+export const products: Product[] = [
+  {
+    id: 1,
+    title: "Carro de mão",
+    description: "Descrição do produto 1",
+    category: "Carro de mão",
+    images: [CarroDeMao],
+  },
+  {
+    id: 2,
+    title: "Carro Jerica 160L",
+    description: "Descrição do produto 2",
+    category: "Carro de mão",
+    images: [CarroJerica160],
+  },
+];
---
+++ src/data/types.ts
@@ -0,0 +1,9 @@
+import { StaticImageData } from "next/image";
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  images: StaticImageData[];
+}
