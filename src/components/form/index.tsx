e9ce9e724498e3e430a7e46bb871fe8504a4ad48
jukalbf <lgbr212@gmail.com>
2025-08-29 14:01:05 -0300

    style(form): formulário estruturado

---
+++ src/components/form/index.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import styles from "./styles.module.scss";
+
+const Form = () => {
+  const [email, setEmail] = useState<string>("");
+  const [nome, setNome] = useState<string>("");
+  const [mensagem, setMensagem] = useState<string>("");
+  const [empresa, setEmpresa] = useState<string>("");
+  const [produto, setProduto] = useState<string>("");
+
+  return (
+    <form className={styles.formContainer}>
+      <h3>Formulário para contato</h3>
+      <label className={`${styles.inputContainer} ${styles.inputNome}`}>
+        <span>Nome Completo</span>
+        <input type="text" onChange={(e) => setNome(e.target.value)} />
+      </label>
+      <label className={`${styles.inputContainer} ${styles.inputEmail}`}>
+        <span>Email</span>
+        <input type="email" onChange={(e) => setEmail(e.target.value)} />
+      </label>
+      <label className={`${styles.inputContainer} ${styles.inputProduto}`}>
+        <span>Produto</span>
+        <input type="text" onChange={(e) => setProduto(e.target.value)} />
+      </label>
+      <label className={`${styles.inputContainer} ${styles.inputMensagem}`}>
+        <span>Mensagem</span>
+        <textarea
+          className={styles.textarea}
+          onChange={(e) => setMensagem(e.target.value)}
+        />
+      </label>
+      <label className={`${styles.inputContainer} ${styles.inputEmpresa}`}>
+        <span>Empresa</span>
+        <input type="text" onChange={(e) => setEmpresa(e.target.value)} />
+      </label>
+    </form>
+  );
+};
+
+export default Form;
