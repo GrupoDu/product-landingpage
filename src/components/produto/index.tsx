b191e7ff495f0686b91f6de4b45174390e7388e4
jukalbf <lgbr212@gmail.com>
2025-08-29 16:22:24 -0300

    style(produto): estruturando componente

---
+++ src/components/produto/index.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styles from "./styles.module.scss";
+import Image, { StaticImageData } from "next/image";
+import { FaWhatsapp } from "react-icons/fa";
+
+type Props = {
+  nomeProduto: string;
+  imagemProduto: StaticImageData;
+  descricaoProduto: string;
+  altImagem: string;
+};
+
+const Produto = ({
+  nomeProduto,
+  imagemProduto,
+  descricaoProduto,
+  altImagem,
+}: Props) => {
+  return (
+    <div className={styles.produtoContainer}>
+      <Image
+        src={imagemProduto}
+        alt={altImagem}
+        className={styles.imagemProduto}
+      />
+      <h3>{nomeProduto}</h3>
+      <span>Descrição</span>
+      <p>{descricaoProduto}</p>
+      <button className={styles.button}>
+        Entrar em contato <FaWhatsapp color="white" />
+      </button>
+    </div>
+  );
+};
+
+export default Produto;
