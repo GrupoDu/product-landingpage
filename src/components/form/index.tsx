570128f5c2722f441ef733a21f3dee1fef8689ad
jukalbf <lgbr212@gmail.com>
2025-09-01 14:48:47 -0300

    feat(form): adicionando funcionalidade de envio no formulário

--- src/components/form/index.tsx
+++ src/components/form/index.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from "react";
 import styles from "./styles.module.scss";
 import Button from "../button";
+import toast from "react-hot-toast";
 
 const Form = () => {
   const [email, setEmail] = useState<string>("");
@@ -11,25 +12,60 @@ const Form = () => {
   const [empresa, setEmpresa] = useState<string>("");
   const [produto, setProduto] = useState<string>("");
 
+  const handleEmail = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/email", {
+        method: "POST",
+        body: JSON.stringify({ email, nome, mensagem, empresa, produto }),
+      });
+
+      if (res.status === 400)
+        return toast.error("Algo deu errado, estamos resolvendo!");
+
+      toast.success("Recebemos seu pedido com sucesso!");
+    } catch (error) {
+      console.log(error);
+      toast.error("Algo deu errado, tente novamente!");
+    }
+  };
+
   return (
     <div className={styles.formContainer}>
       <h2>Formulário para contato</h2>
-      <form className={styles.formulario}>
+      <form className={styles.formulario} onSubmit={handleEmail}>
         <label className={`${styles.inputContainer} ${styles.inputNome}`}>
           <span>Nome Completo</span>
-          <input type="text" placeholder="Seu nome" onChange={(e) => setNome(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Seu nome"
+            onChange={(e) => setNome(e.target.value)}
+          />
         </label>
         <label className={`${styles.inputContainer} ${styles.inputEmail}`}>
           <span>Email</span>
-          <input type="email" placeholder="Seu email" onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            placeholder="Seu email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <label className={`${styles.inputContainer} ${styles.inputProduto}`}>
           <span>Produto</span>
-          <input type="text" placeholder="Produto de interesse" onChange={(e) => setProduto(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Produto de interesse"
+            onChange={(e) => setProduto(e.target.value)}
+          />
         </label>
         <label className={`${styles.inputContainer} ${styles.inputEmpresa}`}>
           <span>Empresa</span>
-          <input type="text" placeholder="Sua empresa" onChange={(e) => setEmpresa(e.target.value)} />
+          <input
+            type="text"
+            placeholder="Sua empresa"
+            onChange={(e) => setEmpresa(e.target.value)}
+          />
         </label>
         <label className={`${styles.inputContainer} ${styles.inputMensagem}`}>
           <span>Mensagem</span>
@@ -39,7 +75,9 @@ const Form = () => {
             onChange={(e) => setMensagem(e.target.value)}
           />
         </label>
-        <Button gradient={true}>Solicitar produto</Button>
+        <Button type="submit" gradient={true}>
+          Solicitar produto
+        </Button>
       </form>
     </div>
   );
