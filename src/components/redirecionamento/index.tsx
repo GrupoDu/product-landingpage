f0a9617b1fa74e2d2200b12dbdfe33a8fdb35141
jukalbf <lgbr212@gmail.com>
2025-09-01 15:37:02 -0300

    style(redirecionamento): mudando div para link

--- src/components/redirecionamento/index.tsx
+++ src/components/redirecionamento/index.tsx
@@ -3,15 +3,16 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import { LuArrowBigRight } from "react-icons/lu";
+import Link from "next/link";
 
 const Redirecionamento = () => {
   return (
-    <div className={styles.redirecionamento}>
+    <Link href={"https://grupodu.com.br"} className={styles.redirecionamento}>
       <span>Conheça o Grupo Du</span>
       <div className={styles.iconContainer}>
         <LuArrowBigRight color="white" className={styles.icon} />
       </div>
-    </div>
+    </Link>
   );
 };
 
