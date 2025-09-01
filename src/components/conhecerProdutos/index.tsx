8c5c03c809cac9bbb0291ab991bf97656d9c0f06
jukalbf <lgbr212@gmail.com>
2025-09-01 15:05:52 -0300

    feat(conhecerProdutos): download de pdf funcional

---
+++ src/components/conhecerProdutos/index.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import styles from "./styles.module.scss";
+import Button from "../button";
+import { MdFileDownload, MdKeyboardArrowRight } from "react-icons/md";
+import Link from "next/link";
+
+const ConhecendoProdutos = () => {
+  const handleDownload = () => {
+    window.open("catalogo.pdf", "_blank");
+  };
+
+  return (
+    <div className={styles.conhecerProdutos}>
+      <h1>Ainda não conhece nossos produtos?</h1>
+      <div className={styles.buttons}>
+        <Button onClick={handleDownload} gradient={true}>
+          <MdFileDownload color="white" className={styles.icon} />
+          Baixe nosso catálogo completo
+        </Button>
+        <Link href={"https://grupodu.com.br"} className={styles.link}>
+          <Button gradient={false}>
+            Conheça o <span style={{ fontWeight: "bold" }}>GrupoDu</span>{" "}
+            <MdKeyboardArrowRight color="black" className={styles.icon} />
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ConhecendoProdutos;
